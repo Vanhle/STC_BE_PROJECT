5b2b3bb24b8751289933e348f199078fc5178f24
Vanhle <levietanh20033002@gmail.com>
2025-07-16 15:21:44 +0700

    logout function and logic refresh token

--- src/main/java/com/stc/project/exception/GlobalExceptionHandler.java
+++ src/main/java/com/stc/project/exception/GlobalExceptionHandler.java
@@ -2,14 +2,17 @@ package com.stc.project.exception;
 
 import com.stc.project.dto.response.ApiResponse;
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.time.Instant;
 import java.util.List;
@@ -17,10 +20,12 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AppException.class)
     public ResponseEntity<ApiResponse<Void>> handleAppException(AppException ex) {
+        log.info("AppException: {}", ex.getErrorCode().name());
         return ResponseEntity.status(ex.getStatus()).body(
                 ApiResponse.<Void>builder()
                         .timestamp(Instant.now())
@@ -33,6 +38,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse<List<Map<String, String>>>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        log.info("MethodArgumentNotValidException: {}", ex.getBindingResult().getFieldErrors());
         List<Map<String, String>> errors = ex.getBindingResult().getFieldErrors().stream()
                 .map(error -> Map.of(
                         "field", error.getField(),
@@ -102,14 +108,26 @@ public class GlobalExceptionHandler {
         );
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<String>> handleGenericException(Exception ex) {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ApiResponse<String>> handleGenericException(RuntimeException ex) {
         ex.printStackTrace(); // hoặc log.error(...)
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
                 ApiResponse.<String>builder()
                         .timestamp(Instant.now())
                         .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
                         .message("INTERNAL_ERROR")
+                        .data(null)
+                        .build()
+        );
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ApiResponse<String>> handleNoResourceFound(NoResourceFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                ApiResponse.<String>builder()
+                        .timestamp(Instant.now())
+                        .status(HttpStatus.NOT_FOUND.value())
+                        .message("NOT_FOUND")
                         .data(null)
                         .build()
         );
