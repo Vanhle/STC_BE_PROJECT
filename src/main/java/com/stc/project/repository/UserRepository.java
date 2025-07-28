3161a68adb4ec8619f9fdb6099df9a1081f22bac
Kien Nguyen <kienn7704@gmail.com>
2025-07-28 11:47:17 +0700

    Kien CRUD

--- src/main/java/com/stc/project/repository/UserRepository.java
+++ src/main/java/com/stc/project/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.stc.project.repository;
 
 import com.stc.project.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -17,5 +19,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
+
+    Page<User> findByEmailContainingIgnoreCase(String email, Boolean isActive, Pageable pageable);
+    Page<User> findByIsActive(Boolean isActive, Pageable pageable);
+    Page<User> findByEmailContainingIgnoreCaseAndIsActive(String email, Boolean isActive, Pageable pageable);
+    Page<User> findByEmail(String email, Pageable pageable);
 }
 
